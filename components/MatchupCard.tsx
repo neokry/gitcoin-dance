6e08483ed6b2c57a2a99efa58e9c2a35230dec4a
Neokry <neokry.eth@gmail.com>
2021-06-30 18:06:17 -0700

    Adds zkcheckout

--- components/MatchupCard.tsx
+++ components/MatchupCard.tsx
@@ -1,8 +1,18 @@
 import { NFTDataContext } from "@zoralabs/nft-components";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { TournamentDataContext } from "../context/TournamentDataContext";
 import { Fragment } from "react";
 import { NFTDataProvider } from "@zoralabs/nft-components";
+import { CheckoutManager } from "zksync-checkout";
+import { ethers } from "ethers";
+import useSWR from "swr";
+
+export type ZKSyncToken = {
+  id: number;
+  address: string;
+  symbol: string;
+  decimals: number;
+};
 
 export type MatchupCardProps = {
   playerIndex: number;
@@ -10,43 +20,36 @@ export type MatchupCardProps = {
 
 export default function MatchupCard({ playerIndex }: MatchupCardProps) {
   const { tournament } = useContext(TournamentDataContext);
-  if (!tournament.data) return <Fragment />;
+  const swr = useSWR("https://api.zksync.io/api/v0.1/tokens");
+
+  if (!tournament.data || !swr.data) return <Fragment />;
   const { data } = tournament;
+  const tokenList: ZKSyncToken[] = swr.data;
 
-  const Content = () => {
-    const {
-      nft: { data },
-      metadata: { metadata },
-    } = useContext(NFTDataContext);
+  //ZKSync checkout
+  const checkout = async (amount: string, tokenId: number) => {
+    const token = tokenList.find((x) => x.id == tokenId);
 
-    if (!data || !metadata) return <Fragment />;
+    if (!tournament.data || !tournament.data.wallets || !token) return;
 
-    return (
-      <div className="m-4">
-        <div>
-          <img
-            className="w-full h-96 object-cover"
-            src={
-              data && "zoraNFT" in data
-                ? data.zoraNFT.contentURI
-                : metadata.image
-            }
-            alt="image"
-          />
+    const checkoutManager = new CheckoutManager("rinkeby");
+    const address = tournament.data.wallets[playerIndex];
 
-          <h1 className="">{metadata.name}</h1>
-          <p>created by: {data.nft.creator}</p>
-          <p>{metadata.description}</p>
-        </div>
-        <div className="mt-4 flex">
-          <select>
-            <option>Dai</option>
-          </select>
-          <input className="mx-2" placeholder="Amount"></input>
-          <button>Vote</button>
-        </div>
-      </div>
+    const transaction = [
+      {
+        to: address,
+        token: token.symbol,
+        amount: ethers.utils.parseUnits(amount, token.decimals).toHexString(),
+        description: "Gitcoin dance off vote",
+      },
+    ];
+
+    const txHashes = await checkoutManager.zkSyncBatchCheckout(
+      transaction,
+      token.symbol
     );
+
+    const receipt = await checkoutManager.wait(txHashes, "COMMIT");
   };
 
   return (
@@ -54,7 +57,74 @@ export default function MatchupCard({ playerIndex }: MatchupCardProps) {
       id={data.tokenIds ? data.tokenIds[playerIndex].toString() : ""}
       contract={data.tokenAddresses ? data.tokenAddresses[playerIndex] : ""}
     >
-      <Content />
+      <Content checkout={checkout} tokenList={tokenList} />
     </NFTDataProvider>
   );
 }
+
+const Content = ({
+  checkout,
+  tokenList,
+}: {
+  checkout: (amount: string, tokenId: number) => void;
+  tokenList: ZKSyncToken[];
+}) => {
+  const {
+    nft: { data },
+    metadata: { metadata },
+  } = useContext(NFTDataContext);
+
+  const [amount, setAmount] = useState("");
+  const [tokenId, setTokenId] = useState("0");
+
+  if (!data || !metadata) return <Fragment />;
+
+  return (
+    <div className="m-4">
+      <div>
+        <img
+          className="w-full h-96 object-cover"
+          src={
+            data && "zoraNFT" in data ? data.zoraNFT.contentURI : metadata.image
+          }
+          alt="image"
+        />
+
+        <h1 className="">{metadata.name}</h1>
+        <p>created by: {data.nft.creator}</p>
+        <p>{metadata.description}</p>
+      </div>
+      <form
+        className="mt-4 flex"
+        onSubmit={(e) => {
+          e.preventDefault();
+          checkout(amount, parseInt(tokenId));
+        }}
+      >
+        <select
+          value={tokenId}
+          onChange={(e) => {
+            setTokenId(e.target.value);
+          }}
+        >
+          {tokenList.map((token) => (
+            <option key={token.id} value={token.id}>
+              {token.symbol}
+            </option>
+          ))}
+        </select>
+        <input
+          type="number"
+          className="mx-2"
+          placeholder="Amount"
+          onChange={(e) => {
+            setAmount(e.target.value);
+          }}
+          value={amount}
+          name="amount"
+        />
+        <button type="submit">Vote</button>
+      </form>
+    </div>
+  );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,25 @@
 import "tailwindcss/tailwind.css";
 import { TournamentDataProvider } from "../context/TournamnetDataProvider";
 import { MediaConfiguration, Networks } from "@zoralabs/nft-components";
+import { SWRConfig } from "swr";
 import type { AppProps } from "next/app";
+import axios from "axios";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MediaConfiguration networkId={Networks.RINKEBY}>
-      <TournamentDataProvider tournamentId={3} chainId={4}>
-        <Component {...pageProps} />
-      </TournamentDataProvider>
-    </MediaConfiguration>
+    <SWRConfig
+      value={{
+        refreshInterval: 0,
+        fetcher: (resource, init) =>
+          axios.get(resource, init).then((x) => x.data),
+      }}
+    >
+      <MediaConfiguration networkId={Networks.RINKEBY}>
+        <TournamentDataProvider tournamentId={3} chainId={4}>
+          <Component {...pageProps} />
+        </TournamentDataProvider>
+      </MediaConfiguration>
+    </SWRConfig>
   );
 }
 export default MyApp;
