bf6e66dd40aa9710512b57060507966f6d0e7c73
Neokry <patrickgenevich@gmail.com>
2021-08-22 22:35:36 -0700

    Fixes issues with styles as rounds progress

--- components/MatchupThumbnail.tsx
+++ components/MatchupThumbnail.tsx
@@ -5,12 +5,18 @@ import Thumbnail from "./Thumbnail";
 import { NFTDataProvider } from "@zoralabs/nft-components";
 import useRound from "../hooks/useRound";
 
-export default function MatchupThumbnail({ roundNumber, players, isLeading }: { roundNumber: number, players: number[], isLeading: boolean }) {
+export default function MatchupThumbnail({
+  roundNumber,
+  players,
+  isLeading,
+}: {
+  roundNumber: number;
+  players: number[];
+  isLeading: boolean;
+}) {
   const { tournament } = useContext(TournamentDataContext);
   const { isWinner } = useRound(roundNumber);
-  // const finalWinner = (id: number) => roundNumber == 1 && isWinner(id);
-
-  const { finalWinner } = useRound(1);
+  const finalWinner = (id: number) => roundNumber == 1 && isWinner(id);
 
   if (!tournament.data) return <Fragment />;
   const { data } = tournament;
@@ -20,13 +26,29 @@ export default function MatchupThumbnail({ roundNumber, players, isLeading }: { 
 
   return (
     <Fragment>
-      <li className={`relative flex flex-initial justify-center flex-col ${isLeading ? "items-start" : "items-end"} ${isWinner(pid1) && finalWinner(pid1) ? "game-win" : "game-loss"} py-10 tournament-bracket-item`}> {/* if team win add this calss name "game-win" else "game-loss */}
-        <div className={`tournament-bracket-match w-48 ${isWinner(pid1) && finalWinner(pid1) ? "team-win" : "team-loss"}`}> {/* if team win add this calss name "team-win" else "team-loss"*/}
-          <div className={`${finalWinner(pid1) ? 'final-winner' : ''}`}>
-            <h1 className="final-winner-text pb-4 text-white italic font-light text-sm md:text-lg text-center">YOU WIN!</h1>
-            <NFTDataProvider key={roundNumber + "-" + pid1}
+      <li
+        className={`relative flex flex-initial justify-center flex-col ${
+          isLeading ? "items-start" : "items-end"
+        } ${
+          isWinner(pid1) ? "game-win" : "game-loss"
+        } py-10 tournament-bracket-item`}
+      >
+        {/* if team win add this calss name "game-win" else "game-loss */}
+        <div
+          className={`tournament-bracket-match w-48 ${
+            isWinner(pid1) ? "team-win" : "team-loss"
+          }`}
+        >
+          {/* if team win add this calss name "team-win" else "team-loss"*/}
+          <div className={`${finalWinner(pid1) ? "final-winner" : ""}`}>
+            <h1 className="final-winner-text pb-4 text-white italic font-light text-sm md:text-lg text-center">
+              YOU WIN!
+            </h1>
+            <NFTDataProvider
+              key={roundNumber + "-" + pid1}
               id={data.tokenIds ? data.tokenIds[pid1].toString() : ""}
-              contract={data.tokenAddresses ? data.tokenAddresses[pid1] : ""}>
+              contract={data.tokenAddresses ? data.tokenAddresses[pid1] : ""}
+            >
               <Link href={`round/${roundNumber}/${pid1}`} passHref={true}>
                 <a>
                   <Thumbnail isWinner={isWinner(pid1)} />
@@ -39,13 +61,29 @@ export default function MatchupThumbnail({ roundNumber, players, isLeading }: { 
           <h1 className="primary-color italic font-light text-lg">VS.</h1>
         </div>
       </li>
-      <li className={`relative flex flex-initial justify-center flex-col ${isLeading ? "items-start" : "items-end"} ${isWinner(pid2) && finalWinner(pid2) ? "game-win" : "game-loss"} py-10 tournament-bracket-item`}> {/* if team win add this calss name "game-win" else "game-loss */}
-        <div className={`tournament-bracket-match w-48 ${isWinner(pid2) && finalWinner(pid2) ? "team-win" : "team-loss"}`}> {/* if team win add this calss name "team-win" else "team-loss"*/}
-          <div className={`${finalWinner(pid2) ? 'final-winner' : ''}`}>
-            <h1 className="final-winner-text pb-4 text-white italic font-light text-sm md:text-lg text-center">YOU WIN!</h1>
-            <NFTDataProvider key={roundNumber + "-" + pid2}
+      <li
+        className={`relative flex flex-initial justify-center flex-col ${
+          isLeading ? "items-start" : "items-end"
+        } ${
+          isWinner(pid2) ? "game-win" : "game-loss"
+        } py-10 tournament-bracket-item`}
+      >
+        {/* if team win add this calss name "game-win" else "game-loss */}
+        <div
+          className={`tournament-bracket-match w-48 ${
+            isWinner(pid2) ? "team-win" : "team-loss"
+          }`}
+        >
+          {/* if team win add this calss name "team-win" else "team-loss"*/}
+          <div className={`${finalWinner(pid2) ? "final-winner" : ""}`}>
+            <h1 className="final-winner-text pb-4 text-white italic font-light text-sm md:text-lg text-center">
+              YOU WIN!
+            </h1>
+            <NFTDataProvider
+              key={roundNumber + "-" + pid2}
               id={data.tokenIds ? data.tokenIds[pid2].toString() : ""}
-              contract={data.tokenAddresses ? data.tokenAddresses[pid2] : ""}>
+              contract={data.tokenAddresses ? data.tokenAddresses[pid2] : ""}
+            >
               <Link href={`round/${roundNumber}/${pid2}`} passHref={true}>
                 <a>
                   <Thumbnail isWinner={isWinner(pid2)} />
@@ -56,5 +94,5 @@ export default function MatchupThumbnail({ roundNumber, players, isLeading }: { 
         </div>
       </li>
     </Fragment>
-  )
+  );
 }
